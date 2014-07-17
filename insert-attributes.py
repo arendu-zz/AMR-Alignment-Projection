fe0a535d6e6e8fccf2839ef1ee28faefc82a2490
arendu <adithya.r@gmail.com>
2014-07-17 16:59:31 +0200

    insert attributes from multiple files

--- insert-attributes.py
+++ insert-attributes.py
@@ -5,14 +5,23 @@ from AMRMetadata import AMRMetadata
 
 if __name__ == '__main__':
     opt = OptionParser()
-    opt.add_option("-f", dest="attribute_file", help="a file with some attribute (single attribute per line)", )
+    opt.add_option("-f", dest="attribute_files", help="files with some attribute (single attribute per line) comma "
+                                                      "seperated if multiple files", )
     opt.add_option("-a", dest="amr_file", help="the amr file that is going to accept the new attribute",
                    default="data/Little_Prince/test.aligned")
-    opt.add_option("-n", dest="attribute_name", help="name of the attribute that is being added")
+    opt.add_option("-n", dest="attribute_names", help="name of the attribute that is being added, comma seperated for "
+                                                      "multiple attributes")
     (options, args) = opt.parse_args()
-    for item, attr in zip(open(options.amr_file, 'r').read().split('\n\n'),
-                          open(options.attribute_file, 'r').readlines()):
+    attr_files = options.attribute_files.split(',')
+    attr_list = []
+    attr_names = options.attribute_names.split(',')
+    for af in attr_files:
+        attr_list.append(open(af, 'r').readlines())
+
+    for idx, item in enumerate(open(options.amr_file, 'r').read().split('\n\n')):
         if item.strip() != '':
             c = AMRMetadata(item)
-            c.add_attribute(options.attribute_name, attr.strip())
+            for al, an in zip(attr_list, attr_names):
+                attr = al[idx]
+                c.add_attribute(an, attr.strip())
             sys.stdout.write(str(c) + '\n')
