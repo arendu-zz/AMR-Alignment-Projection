d6d98d3dbcd6b9108be5436c8975c00859c12534
arendu <adithya.r@gmail.com>
2014-07-21 10:32:47 +0200

    added join file and run shell script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python project-alignments.py -f data/Little_Prince/amr-bank-struct-v1.3.txt.en-aligned > data/Little_Prince/amr-bank-struct-v1.3.txt.en-aligned.caveman
+
+python extract-attributes.py -f data/Little_Prince/amr-bank-struct-v1.3.txt.en-aligned -a zh > data/Little_Prince/zh.unseg
+
+python extract-attributes.py -f data/Little_Prince/amr-bank-struct-v1.3.txt.en-aligned.caveman -a caveman_string > data/Little_Prince/caveman.segmented
+
+segment.sh ctb data/Little_Prince/zh.unseg UTF-8 0 > data/Little_Prince/zh.segmented
+
+rm  data/Little_Prince/zh.unseg
+
+python join.py -z data/Little_Prince/zh.segmented  -c data/Little_Prince/caveman.segmented  > data/Little_Prince/zh-caveman
+
