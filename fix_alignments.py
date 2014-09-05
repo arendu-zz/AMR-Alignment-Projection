7252f062ec1af40acb034fe16738084b30939743
arendu <adithya.r@gmail.com>
2014-09-05 18:57:12 -0400

    got priority que based search working for test alignment

--- fix_alignments.py
+++ fix_alignments.py
@@ -8,19 +8,20 @@ Then the script enforces the alignments to comply to 2 constrains
 see : https://www.writelatex.com/articles/phrase-projection-for-cross-lingual-jamr-training/ttrvffrkrtjy#.VAh9_mRdUpw
 """
 from optparse import OptionParser
+from pprint import pprint
+from heapq import heapify, heappop, heappush
 from AMRMetadata import AMRMetadata
 import pdb
 
 
 def check_alignment(word_alignment, target_spans):
-    word_alignment = word_alignment.strip()
-    target_spans = target_spans.strip()
+    if len(word_alignment) == 0:
+        return False
     # s2t.keys = token index in source
     # s2t.val = token index in target
     s2t = {}
     t2s = {}
-    for st in word_alignment.strip().split():
-        s, t = st.split('-')
+    for s, t in word_alignment:
         t_list = s2t.get(int(s), [])
         s_list = t2s.get(int(t), [])
         t_list.append(int(t))
@@ -28,49 +29,96 @@ def check_alignment(word_alignment, target_spans):
         s2t[int(s)] = t_list
         t2s[int(t)] = s_list
 
-    target_spans = [(int(t.split('-')[0]), t.split('|')[0]) for t in target_spans.split()]
     target_spans.sort()
-    target_spans = [t[1] for t in target_spans]
-    idxspans = [(idx, range(int(t.split('|')[0].split('-')[0]), int(t.split('|')[0].split('-')[1]))) for idx, t in enumerate(target_spans)]
+    target_spans = [t[0] for t in target_spans]
+    ts2s = {}  # target spans to source alignments
+
+    idxspans = [(idx, range(t[0], t[1])) for idx, t in enumerate(target_spans)]
     # target_token_to_span.keys  = token idx of target token
     # target_token_to_span.val = span idx to while the token belongs
-    target_token_to_span = {}
+    target2spanid = {}
     for idx, span in idxspans:
         for s in span:
-            target_token_to_span[s] = idx
+            target2spanid[s] = idx
 
-    source_token_to_span = {}
+    src2targetspans = {}
     for src in range(max(s2t) + 1):  # iterate over source tokens and assign their target phrase membership
         for t in s2t.get(src, []):
-            if target_token_to_span.get(t) is not None:
-                tp_set = source_token_to_span.get(src, set([]))
-                tp_set.add(target_token_to_span.get(t))
-                source_token_to_span[src] = tp_set
-    print source_token_to_span
+            if target2spanid.get(t) is not None:
+                tp_set = src2targetspans.get(src, set([]))
+                tp_set.add(target2spanid.get(t))
+                src2targetspans[src] = tp_set
+
     for ts in target_spans:
-        tokens = range(int(ts.split('-')[0]), int(ts.split('-')[1]))
-        src = t2s.get(tokens[0], None)
-        src = src[0]  # assuming model 1 style alignments TODO what about giza++ alignments??
-        tp_set = source_token_to_span[src]
-        for tok in tokens[1:]:
-            # 3 should map to just 2
-            # 4 shoudl map to 1,2
-            if tp_set != source_token_to_span[t2s.get(tok, None)[0]]:
-                return False
+        tokens = range(ts[0], ts[1])
+        src = None
+        for t in tokens:
+            if t2s.get(t, None) is not None:
+                src = t2s[t]
+                break
+        if src is None:
+            return False  # all alignments from a target phrase have been lost
+        else:
+            src = src[0]  # assuming model 1 style alignments TODO what about giza++ alignments??
+            tp_set = src2targetspans[src]
+            for tok in tokens[1:]:
+                # 3 should map to just 2
+                # 4 shoudl map to 1,2
+                if t2s.get(tok, None) is not None:
+                    if tp_set != src2targetspans[t2s.get(tok, None)[0]]:
+                        return False
 
     return True
 
 
-def remove_alignment(a, A):
+def remove_alignment(a, word_alignment, src_sentence=None, target_sentence=None):
+    new_word_alignment = [p for p in word_alignment if p != a]
+    return len(new_word_alignment), new_word_alignment  # score, alignment
+
+
+def search_alignments(init_alignment, target_spans):
+    accepted_alignments = []
+    Q = []
+    heappush(Q, (-len(init_alignment), init_alignment))  # len(A) is sub for score of an alignment which we want to max/minimize
+    while len(Q) > 0:
+        score_alignment, alignment = heappop(Q)
+        if not check_alignment(alignment, target_spans):
+            for a in alignment:
+                score_new_alignment, new_alignment = remove_alignment(a, alignment)
+                heappush(Q, (-score_new_alignment, new_alignment))
+        else:
+            accepted_alignments.append((score_alignment, alignment))
+            break
+    return accepted_alignments
+
+
+def test():
+    t_spans = [((0, 2), "X"), ((2, 4), "Y"), ((4, 5), "Z")]
+    w_aligns = [(2, 0), (1, 1), (4, 2), (4, 3), (4, 4)]
+    assert check_alignment(w_aligns, t_spans) == True
+
+    t_spans = [((0, 2), "X"), ((2, 4), "Y"), ((4, 5), "Z")]
+    w_aligns = [(2, 0), (1, 1), (3, 2), (4, 3), (4, 4)]
+    assert check_alignment(w_aligns, t_spans) == False
+
+    t_spans = [((0, 2), "X"), ((2, 4), "Y"), ((4, 5), "Z")]
+    w_aligns = [(2, 0), (3, 2), (4, 3), (4, 4)]
+    assert check_alignment(w_aligns, t_spans) == False
+
+    t_spans = [((0, 2), "X"), ((2, 4), "Y"), ((4, 5), "Z")]
+    w_aligns = [(2, 0), (3, 2), (4, 3)]
+    assert check_alignment(w_aligns, t_spans) == False
     pass
 
 
 if __name__ == '__main__':
+    test()
     opt = OptionParser()
     opt.add_option("-a", dest="src_target_alignments", help="source-target alignments (of full sentences) from fast_align",
                    default="data/Little_Prince/zh-en.alignments")
 
     opt.add_option("-f", dest="amr_file", help="amr file with target phrases", default="data/Little_Prince/amr-bank-struct-v1.3.txt.en-aligned")
+    opt.add_options("-l", dest="lex_file", help="lexical translation probs", default=)
     (options, args) = opt.parse_args()
 
     word_alignments = open(options.src_target_alignments, 'r').readlines()
@@ -84,15 +132,15 @@ if __name__ == '__main__':
             # sometimes there are no alignments that's why there is a default
             # of ''
             spans = c.attributes.get('alignments', '')
+            #spans = [(int(t.split('-')[0]), t.split('|')[0]) for t in target_spans.strip().split()]
             check_alignment(word_alignment, spans)
     """
-    t_spans = "0-2|X 2-4|Y 4-5|Z"
-    w_aligns = "2-0 1-1 3-2 4-3 4-4"
-    assert check_alignment(w_aligns, t_spans) == False
 
-    t_spans = "0-2|X 2-4|Y 4-5|Z"
-    w_aligns = "2-0 1-1 4-2 4-3 4-4"
-    assert check_alignment(w_aligns, t_spans) == True
+    spans = [((0, 2), "X"), ((2, 4), "Y"), ((4, 5), "Z")]
+    A = [(2, 0), (1, 1), (3, 2), (4, 3), (4, 4)]
+    print search_alignments(A, spans)
+
+
 
 
 
