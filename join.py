189d0b31e8d2a320c0ae02c8241bb20c77b5bb3f
arendu <adithya.r@gmail.com>
2014-09-08 15:21:36 -0400

    changed help description

--- join.py
+++ join.py
@@ -8,7 +8,7 @@ because sed s/\t/ ||| /g' is doing something funny
 if __name__ == '__main__':
     opt = OptionParser()
     opt.add_option("-z", dest="zh_file", help="zh file", default="data/Little_Prince/zh.segmented")
-    opt.add_option("-c", dest="caveman_file", help="en file", default="data/Little_Prince/caveman.segmented")
+    opt.add_option("-c", dest="en_file", help="en file", default="data/Little_Prince/caveman.segmented")
     (options, args) = opt.parse_args()
-    for z, c in zip(open(options.zh_file, 'r').readlines(), open(options.caveman_file, 'r').readlines()):
+    for z, c in zip(open(options.zh_file, 'r').readlines(), open(options.en_file, 'r').readlines()):
         print z.strip() + ' ||| ' + c.strip()
